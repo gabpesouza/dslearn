9d0c9d7ce85cd50a3ee4b4da75988015edd79bb7
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2022-10-24 12:11:07 -0300

    Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,4 +15,4 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Spring', 'https://prod-discovery.edx-cdn.org/media/course/image/156313d6-f892-4b08-9cee-43ea582f4dfb-7b98c686abcc.small.png', 'https://inpaonline.com.br/wp-content/uploads/2019/10/online-course-8-899132.png');
