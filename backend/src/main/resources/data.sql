7d16b62365135f81a50c1cbe0782a27a74c62050
Gabriel Pereira <gabriel.psouza1997@outlook.com>
2022-10-24 12:58:42 -0300

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -16,3 +16,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp Spring', 'https://prod-discovery.edx-cdn.org/media/course/image/156313d6-f892-4b08-9cee-43ea582f4dfb-7b98c686abcc.small.png', 'https://inpaonline.com.br/wp-content/uploads/2019/10/online-course-8-899132.png');
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-05-23T20:50:05Z', TIMESTAMP WITH TIME ZONE '2021-05-23T20:50:05Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-23T20:50:05Z', TIMESTAMP WITH TIME ZONE '2021-12-23T20:50:05Z', 1);
